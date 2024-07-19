08fc242fb11b4bf5035152b008420d9f5afeef93
mandeep <minturajmdb1999@gmail.com>
2024-07-19 20:43:52 +0530

    update

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,56 +1,70 @@
-import {  useEffect, useState } from "react"
+import { useState } from "react";
 
 const Form = () => {
-    
-     const [usdt,setUsdt]=useState("");
-     const [ftm,setFtm]=useState("");
-      
-     
-   function handleChange()
-   {
-      
-   }
+  const [usdt, setUsdt] = useState("");
+  const [ftm, setFtm] = useState("");
+
+  const handleChange = (e:any) => {
+    const { name, value } = e.target;
+    if (name === "usdt") {
+      setUsdt(value);
+    } else if (name === "ftm") {
+      setFtm(value);
+    }
+  };
+
+  const getInputStyle = (data:any ) => ({
+    color: data && parseFloat(data) < 100 ? "red" : "white",
+  });
 
   return (
-    
-      
-    <form className='form' >
-        <div className='FormChild'>
-                <div className='formLabel'>
-                  <label>USDT</label>
-                 <label>Available 0000</label>
-                </div>
-                <div>
-                  <input type='text' value={usdt} onChange={handleChange}/>
-                </div>
-                <span > 
-                    <label>0%</label>
-                    <label>25%</label>
-                    <label>50%</label>
-                    <label>75%</label>
-                    <label>MAX</label>
-                </span>
+    <form className="form">
+      <div className="FormChild">
+        <div className="formLabel">
+          <label>USDT</label>
+          <label>Available 0000</label>
         </div>
-        <div className='FormChild'>
-                <div className='formLabel'>
-                  <label>FTM</label>
-                 <label>Available 0000</label>
-                </div>
-                <div>
-                  <input type='text' value={ftm} onChange={handleChange} />
-                </div>
-                <span > 
-                   <label>0%</label>
-                    <label>25%</label>
-                    <label>50%</label>
-                    <label>75%</label>
-                    <label>MAX</label>
-                    </span>
+        <div>
+          <input
+            type="text"
+            name="usdt"
+            value={usdt}
+            onChange={handleChange}
+            style={getInputStyle(usdt)}
+          />
         </div>
+        <span>
+          <label>0%</label>
+          <label>25%</label>
+          <label>50%</label>
+          <label>75%</label>
+          <label>MAX</label>
+        </span>
+      </div>
+      <div className="FormChild">
+        <div className="formLabel">
+          <label>FTM</label>
+          <label>Available 0000</label>
+        </div>
+        <div>
+          <input
+            type="text"
+            name="ftm"
+            value={ftm}
+            onChange={handleChange}
+            style={getInputStyle(ftm)}
+          />
+        </div>
+        <span>
+          <label>0%</label>
+          <label>25%</label>
+          <label>50%</label>
+          <label>75%</label>
+          <label>MAX</label>
+        </span>
+      </div>
     </form>
+  );
+};
 
-    
-  )
-}
-
-export default Form
+export default Form;
